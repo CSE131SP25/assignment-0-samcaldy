4b06f23a8683cb3b563bc80e10b8e12ca2ea4ac9
samcalderwood <samcalderwood@localhost>
2025-01-21 17:21:29 -0600

    Drew a flower.

--- src/assignment0/RobotInstructions.java
+++ src/assignment0/RobotInstructions.java
@@ -29,11 +29,72 @@ public class RobotInstructions  {
 		 * You will add instructions at the end of the code to cause the support.robot to move
 		 * as you desire.
 		 */
-		robot.forward(60);
-		robot.turnLeft(90);
-		robot.forward(23);
+		robot.forward(200);
 		robot.turnRight(90);
-		robot.forward(80);
+		robot.forward(50);
+		robot.turnLeft(45);
+		
+		robot.forward(100);
+		robot.turnLeft(45);
+		
+		robot.forward(100);
+		robot.turnLeft(45);
+		
+		robot.forward(100);
+		robot.turnLeft(45);
+		
+		robot.forward(100);
+		robot.turnLeft(45);
+		
+		robot.forward(100);
+		robot.turnLeft(45);
+		
+		robot.forward(100);
+		robot.turnLeft(45);
+		
+		robot.forward(100);
+		robot.turnLeft(45);
+		
+		robot.forward(100);
+		
+		robot.forward(71);
+		robot.turnLeft(90);
+		robot.forward(71);
+		
+		robot.turnRight(45);
+		robot.forward(71);
+		robot.turnLeft(90);
+		robot.forward(71);
+		
+		robot.turnRight(45);
+		robot.forward(71);
+		robot.turnLeft(90);
+		robot.forward(71);
+		
+		robot.turnRight(45);
+		robot.forward(71);
+		robot.turnLeft(90);
+		robot.forward(71);
+		
+		robot.turnRight(45);
+		robot.forward(71);
+		robot.turnLeft(90);
+		robot.forward(71);
+		
+		robot.turnRight(45);
+		robot.forward(71);
+		robot.turnLeft(90);
+		robot.forward(71);
+		
+		robot.turnRight(45);
+		robot.forward(71);
+		robot.turnLeft(90);
+		robot.forward(71);
+		
+		robot.turnRight(45);
+		robot.forward(71);
+		robot.turnLeft(90);
+		robot.forward(71);
 	}
 
 }
